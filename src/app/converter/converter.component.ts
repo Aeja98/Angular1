ef83a1a4532057c07d2823dda16e3b9dd2b4d949
Angelica Adolfsson <angelicaadolfsson1gmail.com>
2025-05-01 17:04:51 +0200

    updated conversion component -Can switch between the different conversions via toggle switch -Added cm to in conversion -Styled toggle and conversion page
    Added Result-box component (imported to ts file)
    
    Added some colors to theme.scss

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,26 +1,50 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ResultBoxComponent } from './result-box/result-box.component';
 
 @Component({
   selector: 'app-converter',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ResultBoxComponent],
   templateUrl: './converter.component.html',
-  styleUrl: './converter.component.scss'
+  styleUrls: ['./converter.component.scss']
+
 })
 export class ConverterComponent {
+  convertFeetMode: boolean = true;
   meters: number = 0;
   feetResult: number | null = null;
 
+  convertTempMode: boolean = true;
   celsius: number = 0;
   fahrenheitResult: number | null = null;
 
-  convertToFeet(): void {
-    this.feetResult = this.meters * 3.28084; // 1 meter = 3.28084 feet
+  convertMeasurementMode: boolean = true;
+  centimeters: number = 0;
+  inchesResult: number | null = null;
+
+  convertDistance(): void {
+    if (this.convertFeetMode) {
+      this.feetResult = this.meters * 3.28084; // ft = m * 3.28084
+    } else {
+      this.feetResult = this.meters / 3.28084; // m = ft/3.28084
+    }
   }
 
-  convertToFahrenheit(): void {
-    this.fahrenheitResult = (this.celsius * 9/5) + 32; // °C to °F formula
+  convertTemperature(): void {
+    if (this.convertTempMode) {
+      this.fahrenheitResult = (this.celsius * 9/5) + 32; // F = (C * 9/5) +32
+    } else {
+      this.fahrenheitResult = (this.celsius - 32) * 5/9; // C = (F - 32) * 5/9
+    }
+  }
+
+  convertMeasurement(): void {
+    if (this.convertMeasurementMode) {
+      this.inchesResult = this.centimeters / 2.54; // in = cm/2.54
+    } else {
+      this.inchesResult = this.centimeters * 2.54; // cm = in * 2.54
+    }
   }
 }

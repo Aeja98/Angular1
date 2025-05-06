3b8212dda96eeb7f87a9c08df405e2f8a3c7a893
Angelica Adolfsson <angelicaadolfsson1gmail.com>
2025-05-06 15:41:30 +0200

    Fixed menu/nav issues, styled & responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 
@@ -13,7 +15,19 @@ export class AppComponent {
   title = 'Angular1';
   menuOpen: boolean = false;
 
-  toggleMenu(): void {
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.closeMenu();
+      });
+  }
+
+  toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 }
